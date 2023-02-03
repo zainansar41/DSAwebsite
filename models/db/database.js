f842cd156d32deb2f0e6b3eb635ba56acdfca116
ZAIN UL ABIDIN <108021130+zainansar41@users.noreply.github.com>
2023-02-03 10:37:28 +0000

    updated

--- models/db/database.js
+++
@@ -1,5 +0,0 @@
-const mongoose= require('mongoose')
-const URI=process.env.DB_uri
-const data=mongoose.createConnection(URI)
-
-module.exports=data
--- models/handlers/cloudinary.js
+++
@@ -1,8 +0,0 @@
-const cloudinary=require('cloudinary')
-
-cloudinary.config({
-    cloud_name:process.env.CLOUD_NAME,
-    api_key:process.env.API_ID,
-    api_secret:process.env.API_SECRET
-})
-module.exports=cloudinary
